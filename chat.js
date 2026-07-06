54a33b4779643f9d2fbf6a1214e86a46e25b11fc
inovasijimj <inovasi.jimj@imi.gov.my>
2026-07-06 13:22:18 +0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,25 +1,34 @@
-function replyAI() {
+function replyAI(){
 
-    const question = document.getElementById("question").value.trim();
+    const question=document.getElementById("question").value.trim();
 
-    if(question===""){
-        return;
-    }
+    if(question=="") return;
 
-    const result = findAnswer(question);
+    document.getElementById("answer").innerHTML=`
+    <div style="padding:15px;">
+    🤖 AI sedang berfikir...
+    </div>
+    `;
 
-    document.getElementById("answer").innerHTML = `
-    <div style="background:#eef7ff;
-                padding:15px;
-                border-left:5px solid #0b5ed7;
-                border-radius:8px;">
+    setTimeout(function(){
 
-        🤖 <b>MyPDT Smart AI</b><br><br>
+        const result=findAnswer(question);
+
+        document.getElementById("answer").innerHTML=`
+        <div style="
+        background:#eef7ff;
+        border-left:5px solid #0b5ed7;
+        padding:15px;
+        border-radius:8px;">
+
+        🤖 <b>${result.title}</b><br><br>
 
         ${result.answer}
 
-    </div>
-    `;
+        </div>
+        `;
+
+    },700);
 
     document.getElementById("question").value="";
 }
