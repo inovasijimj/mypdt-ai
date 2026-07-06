47e5a36fe84f320ba3f5a79558227c51bd94272a
inovasijimj <inovasi.jimj@imi.gov.my>
2026-07-06 13:31:05 +0800

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,7 +1,83 @@
 function findAnswer(question){
 
     const q = question.toLowerCase().trim();
+// Salam
+if(q.includes("assalamualaikum") || q=="salam"){
 
+    return{
+        title:"Salam",
+        answer:`
+Waalaikumussalam 😊
+
+Selamat datang ke MyPDT Smart AI.
+
+Saya boleh membantu anda berkaitan:
+
+* Kelayakan
+* Dokumen
+* Waktu Operasi
+* Tempahan Slot
+* Drive-In
+* Walk-In
+* Hubungi Kami
+
+Sila taip soalan anda.
+`
+    };
+
+}
+
+// Hello
+if(q.includes("hello") || q.includes("hai") || q.includes("hi")){
+
+    return{
+        title:"Hello",
+        answer:`
+Hai 😊
+
+Saya ialah MyPDT Smart AI.
+
+Apa yang ingin anda ketahui mengenai MyPassport Drive-In & Take?
+`
+    };
+
+}
+
+// Terima kasih
+if(q.includes("terima kasih") || q.includes("thanks")){
+
+    return{
+        title:"Sama-sama",
+        answer:`
+😊 Sama-sama.
+
+Semoga urusan anda dipermudahkan.
+
+Jika ada soalan lain mengenai MyPDT, sila tanya saya.
+`
+    };
+
+}
+   if(q.includes("orang tua")){
+    q+=" warga emas";
+}
+
+if(q.includes("oku")){
+    q+=" orang kurang upaya";
+}
+
+if(q.includes("mengandung")){
+    q+=" wanita hamil";
+}
+
+if(q.includes("booking")){
+    q+=" tempahan";
+}
+
+if(q.includes("book")){
+    q+=" tempahan";
+}
+    
     for(const key in knowledge){
 
         const item = knowledge[key];
